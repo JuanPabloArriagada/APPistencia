b738f95068de9b8ca8bdf53d784721d18ae50698
Akxlarre <b.rebolledo@duocuc.cl>
2024-11-09 20:50:05 -0300

    2131

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -28,8 +28,8 @@ export class RegistroPage implements OnInit {
     private fb: FormBuilder
   ) {
     this.registroForm = this.fb.group({
-      rut: ['', Validators.required, Validators.maxLength(12)], // Rut sin puntos ni guión
-      Nombre: ['', Validators.required, Validators.maxLength(40)], // Nombre completo
+      rut: ['', [Validators.required, Validators.maxLength(12)]], // Rut sin puntos ni guión
+      Nombre: ['', [Validators.required, Validators.maxLength(40)]], // Nombre completo
       correo: ['', [Validators.required, Validators.email, Validators.maxLength(50), Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)]], // Correo electrónico
       contrasena: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/)]], // Al menos 6 caracteres, una mayúscula, una minúscula y un número y sin espacios
       rol: [{ value: '', disabled: true }],  
